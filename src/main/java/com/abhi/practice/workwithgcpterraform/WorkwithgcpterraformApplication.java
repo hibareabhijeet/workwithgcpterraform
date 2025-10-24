89fc1fa84a3274456c43aaf8af830d4fa044842e
ahibare <abhijit.hibare2@ingka.com>
2025-10-24 14:52:10 +0200

    Initial commit: Spring Boot app for GCP Cloud Run

---
+++ src/main/java/com/abhi/practice/workwithgcpterraform/WorkwithgcpterraformApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.practice.workwithgcpterraform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkwithgcpterraformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkwithgcpterraformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhi/practice/workwithgcpterraform/WorkwithgcpterraformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.practice.workwithgcpterraform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkwithgcpterraformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
